72e57907f10ccdaac79da32f90e9005a559655eb
Fernando <dwk262@gmail.com>
2026-02-04 12:54:16 +0100

    Inicio de tema de persitencias

--- Persistencia-Navegacion-IOS/ContentView.swift
+++ Persistencia-Navegacion-IOS/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // definimos una persistencia
+    // si exiten los datos los ignora a no ser,
+    // que se genere una función para tal efecto
+    @AppStorage("usuario") private var nombreUsuario = "Invitado"
+    @AppStorage("musica aCtiva") private var musicaActiva: Bool = false
+    // se puede implementar un array
+    // pero lo idela es simplicar para pdoer acceder de forma fmás fácil
+    
+    // para otro tipo de persistencias se usa otra cosa más adelante
+    // con otras estructuras persistentes en el dispositivo
+    // swift data core
+    // ORM etc...
+    
+    // https://developer.apple.com/documentation/swiftdata
+    
+    // datos de app online / cache de ddatos temporales ...
+    // https://developer.apple.com/documentation/coredata
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section("Datos de usuario - persistentes en el dispositivo -") {
+                TextField("Tu nopmbre", text: $nombreUsuario)
+                Toggle("Musica activada", isOn: $musicaActiva)
+            }
         }
-        .padding()
     }
 }
 
