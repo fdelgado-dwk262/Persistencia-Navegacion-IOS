901b08b5fc4d60171385ee9c1175c0520709dc5c
Fernando <dwk262@gmail.com>
2026-02-04 12:25:02 +0100

    Initial Commit

---
+++ Persistencia-Navegacion-IOS/Persistencia_Navegacion_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Persistencia_Navegacion_IOSApp.swift
+//  Persistencia-Navegacion-IOS
+//
+//  Created by Equipo 9 on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Persistencia_Navegacion_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
