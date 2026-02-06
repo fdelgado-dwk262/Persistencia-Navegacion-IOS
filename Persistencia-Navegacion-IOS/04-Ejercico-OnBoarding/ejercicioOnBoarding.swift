43b974411fbcd470e3f5c4d78fd7006b5453d693
Fernando <dwk262@gmail.com>
2026-02-06 11:34:07 +0100

    Ejercicio del profe

---
+++ Persistencia-Navegacion-IOS/04-Ejercico-OnBoarding/ejercicioOnBoarding.swift
@@ -0,0 +1,18 @@
+//
+//  ejercicioOnBoarding.swift
+//  Persistencia-Navegacion-IOS
+//
+//  Created by Equipo 9 on 6/2/26.
+//
+
+import SwiftUI
+
+struct ejercicioOnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ejercicioOnBoarding()
+}
